49e4829d4014f5ceb46f7d2722e3a0f974006af3
studentmini1 <dfdsdfgh@>
2026-03-23 14:11:30 +0100

    witryna administracyjna

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Post
+
+
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'publish', 'author']
+    list_filler = ['status', 'publish', 'created', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
